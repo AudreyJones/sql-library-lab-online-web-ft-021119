12ca931c0b275c1dc091fbf0dd2f582ac0a0d6e4
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-22 15:50:41 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,60 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year
+  FROM books
+  JOIN series
+  ON series.id = books.series_id
+  WHERE series.id = 1
+  ORDER BY(books.year)"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto
+  FROM characters
+  GROUP BY(characters.motto)
+  HAVING MAX(LENGTH(characters.motto))
+  LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species)
+  FROM characters
+  GROUP BY(characters.species)
+  ORDER BY(characters.species) DESC
+  LIMIT 1"
 end
 
-def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+def select_name_and_series_subgenres_of_authors #Fake a FULL OUTER JOIN?
+  "SELECT authors.name, subgenre.name
+  FROM authors
+  LEFT JOIN series
+  ON authors.id = series.author_id
+  UNION ALL
+  SELECT authors.name, subgenre.name
+  FROM subgenres
+  LEFT JOIN series
+  ON subgenres.id = series.subgenre_id
+  GROUP BY(authors.name)"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  JOIN characters
+  ON series.id = characters.series_id
+  GROUP BY(series.title)
+  ORDER BY(COUNT(characters.species = 'human')) DESC
+  LIMIT 1"
 end
 
-def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+def select_character_names_and_number_of_books_they_are_in #use character_books join table!
+  "SELECT characters.name, COUNT(books.id)
+  FROM characters
+  LEFT JOIN character_books
+  ON characters.id = series.character_id
+  UNION ALL
+  SELECT characters.name, COUNT(books.id)
+  FROM character_books
+  LEFT JOIN books
+  ON books.id = character_books.book_id"
 end
