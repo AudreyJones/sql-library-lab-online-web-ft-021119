a15fb86f99b37f290373b11cbe60408dbb50d341
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-22 16:31:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,11 +27,12 @@ end
 def select_name_and_series_subgenres_of_authors #Fake a FULL OUTER JOIN?
   "SELECT authors.name, subgenres.name
   FROM authors
-  INNER JOIN series
+  JOIN series
   ON authors.id = series.author_id
-  INNER JOIN subgenres
+  JOIN subgenres
   ON series.id = series.subgenre_id
-  GROUP BY(authors.name)"
+  GROUP BY(authors.name)
+"
 end
 
 def select_series_title_with_most_human_characters
